f01ef5a112b5bfbb6b4506837c2b006f92baa738
rungene <rungenesamuel@gmail.com>
2023-05-22 18:59:21 -0700

    extend your Python script to export data in the JSON format.

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+0-gather_data_from_an_API.py module
+Python script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODO list progress."""
+
+from json import dump
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    response = get(url)
+    user_name = response.json().get('name')
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+    response = get(url)
+
+    json_obj = response.json()
+    my_dict = {user_id: []}
+    filename = '{}.json'.format(user_id)
+    for obj in json_obj:
+        my_dict[user_id].append({
+                                "task": obj.get('title'),
+                                "completed": obj.get('completed'),
+                                "username": user_name
+                                })
+    with open(filename, "w") as json_file:
+        dump(my_dict, json_file)
