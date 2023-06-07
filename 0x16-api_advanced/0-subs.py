f081bbfc5800b7d73dd8626ee53e3a23d356b71f
rungene <rungenesamuel@gmail.com>
2023-06-06 19:19:47 -0700

    Corrections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Recursive function queries the Reddit API and returns
     the number of subscribers (not active users, total subscribers)
     for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     # setting a custom User-Agent.Avoids error - Too many request
     headers = {'User-Agent': 'Mozilla/5.0'}
 
