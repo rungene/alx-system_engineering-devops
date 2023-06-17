0b1e57fbdcf9c42c585ac1ca07bf7ad75c898013
rungene <rungenesamuel@gmail.com>
2023-06-17 11:06:27 +0000

    Change file mode to executable

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# Correcting too many open file error
+# Correcting too many open file error nginx
 exec { 'increase ulimit':
   path    => '/bin/',
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
