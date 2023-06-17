21482e89f555911efcd799c2ae3aa267b6fbb5c6
rungene <rungenesamuel@gmail.com>
2023-06-17 10:17:57 +0000

    Using puppet to increase ulimit to 4096 to correct 'too many open files from nginx'

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# Correcting too many open file error
+exec { 'increase ulimit':
+  path    => '/bin/',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+}
+
+exec { 'restart-nginx':
+  path    => '/usr/bin/',
+  command => 'service nginx restart',
+}
