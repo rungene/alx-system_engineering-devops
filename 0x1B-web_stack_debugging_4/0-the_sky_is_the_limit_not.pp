b19383163ba799659fb9b8fb599da097cafb27ee
rungene <rungenesamuel@gmail.com>
2023-06-18 09:59:33 +0000

    Adding require parameter

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,11 @@
 # Correcting too many open file error nginx
-exec { 'increase ulimit':
+exec { 'increase_ulimit':
   path    => '/bin/',
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx'
 }
 
 exec { 'restart-nginx':
   path    => '/usr/bin/',
   command => 'service nginx restart',
+  require => Exec['increase_ulimit']
 }
