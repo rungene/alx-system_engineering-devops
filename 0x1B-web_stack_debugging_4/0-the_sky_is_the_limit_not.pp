993786fd3b75df2edd8a614aba3ecb6fdccdc8a2
rungene <rungenesamuel@gmail.com>
2023-06-20 09:20:53 +0000

    Corrections

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,12 @@
 # Correcting too many open file error nginx
+
+# Increase ulimit
 exec { 'increase_ulimit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
 
+# Restart Nginx
 exec { 'restart-nginx':
   command => 'nginx restart',
   path    => '/etc/init.d/'
